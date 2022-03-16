5e1acd87cf46c2372867ecbfd95f85a779810eee
Tyler Gandy <tylerbgandy@gmail.com>
2022-03-15 20:17:16 -0400

    small change to REsults Model

---
+++ models/Result.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+
+
+const sequelize = require("../config/controller.js");
+const bcrypt = require('bcrypt');
+
+class Result extends Model {}
+
+Result.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+    }
+    },
+    user_answers: [{
+      type: DataTypes.STRING,
+      allowNull: false,
+    }],
+
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'result',
+  }
+);
+
+module.exports = Result;
