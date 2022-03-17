1118630084f61ef294bd4284696e08e3197d4478
Yan Zhang <yannbuda@gmail.com>
2022-03-17 17:29:52 -0400

    I added avatar to the user to make it look nicer. I added the img_url field to the user model and added img_url to userData.json. I added img_num()  helper function to generate a random number from 1-10 for the src of avatar image.

--- controllers/api/myMatchRoutes.js
+++ controllers/api/myMatchRoutes.js
@@ -1,26 +1,26 @@
 const router = require('express').Router();
 const Result = require('../../models/Result');
 
-router.post('/quiz', async (req, res) => {
-  try {
-    // loop over user_answer array
-    const myMatch = await Result.create({
-      user_id: req.session.user_id,
-      user_answer_one: req.body.user_answers[0],
-      user_answer_two: req.body.user_answers[1],
-      user_answer_three: req.body.user_answers[2],
-      user_answer_four: req.body.user_answers[3],
-      user_answer_five: req.body.user_answers[4],
-    });
+router.post('/quiz', async(req, res) => {
+    try {
+        // loop over user_answer array
+        const myMatch = await Result.create({
+            user_id: req.session.user_id,
+            user_answer_one: req.body.user_answers[0],
+            user_answer_two: req.body.user_answers[1],
+            user_answer_three: req.body.user_answers[2],
+            user_answer_four: req.body.user_answers[3],
+            user_answer_five: req.body.user_answers[4],
+        });
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
+        req.session.save(() => {
+            req.session.user_id = userData.id;
 
-      res.status(200).json(myMatch);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+            res.status(200).json(myMatch);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;
